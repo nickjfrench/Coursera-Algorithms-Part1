b88565bcba4e44d6cbd0c56b63635338a8a365e7
Forsak3nBacon <nickjfrench908@gmail.com>
2023-07-18 15:38:27 -0400

    Week 1 Resubmitted

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,6 @@
-package week1;
-
 public class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("Hello and welcome!");
+        System.out.println("Hello, World");
     }
 }
